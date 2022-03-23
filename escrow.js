a4edc84acf4d32b858165c92ab04ec27fee9d79a
barnaby-smith <bs663@cam.ac.uk>
2022-03-23 15:34:56 +0000

    Mostly finished ui

--- escrow.js
+++ escrow.js
@@ -1,1 +1,16 @@
 console.log(ethers);
+
+const betFormElement = document.getElementById("bet-form");
+
+betFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let data = new FormData(betFormElement);
+  const bet = {};
+  for (const [name, value] of data) {
+    bet[name] = value;
+  }
+  console.log(bet);
+  // bet is an object containing all the information about the bet
+  // START CODING HERE
+  /////////////////////////////////////////////////////
+});
